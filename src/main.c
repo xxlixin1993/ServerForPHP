26ceaa179c58e14a3b7cf962d566c742732ea6dd
xxlixin1993 <403136170@qq.com>
2017-08-31 16:55:07 +0800

    test cmake

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "../include/b.h"
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+    double a = 49.0;
+    double b = 0.0;
+
+    printf("input a:%f\n",a);
+    b = cal_sqrt(a);
+    printf("sqrt result:%f\n",b);
+    return 0;
+}
